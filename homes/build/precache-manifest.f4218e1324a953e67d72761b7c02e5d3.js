060db6f0804303eb8b6d3adb13eacb6b5fe5c3f5
Ahmed Osama <ahmedossamasaied@gmail.com>
2020-04-06 01:21:37 +0200

    making build

---
+++ homes/build/precache-manifest.f4218e1324a953e67d72761b7c02e5d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb22a7d21bdab390dcb56b3e6c40cc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ab17d324dd33c8f5f1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "59202f1076ca3617ae21",
+    "url": "/static/js/2.4a540084.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a540084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ab17d324dd33c8f5f1",
+    "url": "/static/js/main.62485abe.chunk.js"
+  },
+  {
+    "revision": "f7af31cf9b9b3093678b",
+    "url": "/static/js/runtime-main.b3b88e15.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
